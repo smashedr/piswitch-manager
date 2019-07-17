d90d8185141bdc0635eb3d384846b9332beb038c
Shane <shane@shanerice.me>
2019-07-16 21:33:54 -0700

    Added basic settings functionality

--- home/forms.py
+++ home/forms.py
@@ -1,17 +1,14 @@
 from django import forms
-from django.conf import settings
-
-import logging
-logger = logging.getLogger('app')
 
 
 class PiSwitchSettings(forms.Form):
-    twitch_user = forms.CharField(max_length=255)
-    redis_pass = forms.CharField(max_length=255)
-    redis_host = forms.CharField(max_length=255)
-    redis_port = forms.IntegerField()
-    wifi_sec_before_auto = forms.IntegerField()
-    wifi_auto_config = forms.BooleanField(required=False)
+    twitch_user = forms.CharField(max_length=255, label='Twitch Username')
+    redis_pass = forms.CharField(max_length=255, label='Redis Password')
+    redis_host = forms.CharField(max_length=255, label='Redis Hostname')
+    redis_port = forms.IntegerField(label='Redis Port')
+    redis_db = forms.IntegerField(label='Redis DB')
+    # wifi_sec_before_auto = forms.IntegerField()
+    # wifi_auto_config = forms.BooleanField(required=False)
 
     def clean_redis_port(self):
         data = self.cleaned_data['redis_port']
@@ -20,9 +17,16 @@ class PiSwitchSettings(forms.Form):
         except:
             raise forms.ValidationError('Please specify a valid port number.')
 
-    def clean_wifi_sec_before_auto(self):
-        data = self.cleaned_data['wifi_sec_before_auto']
+    def clean_redis_db(self):
+        data = self.cleaned_data['redis_db']
         try:
             return int(data)
         except:
-            raise forms.ValidationError('Please specify a valid number of seconds.')
+            raise forms.ValidationError('Please specify a valid db number.')
+
+    # def clean_wifi_sec_before_auto(self):
+    #     data = self.cleaned_data['wifi_sec_before_auto']
+    #     try:
+    #         return int(data)
+    #     except:
+    #         raise forms.ValidationError('Please specify a valid number of seconds.')
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from dotenv import load_dotenv
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from home.forms import PiSwitchSettings
@@ -9,8 +11,14 @@ logger = logging.getLogger('app')
 
 def show_settings(request):
     #  View: /
-    data = {}
+    data = get_settings()
+    logger.debug(data)
     form = PiSwitchSettings()
+    form.fields['twitch_user'].initial = data['TWITCH_USER']
+    form.fields['redis_pass'].initial = data['REDIS_PASS']
+    form.fields['redis_host'].initial = data['REDIS_HOST']
+    form.fields['redis_port'].initial = data['REDIS_PORT']
+    form.fields['redis_db'].initial = data['REDIS_DB']
     return render(request, 'home.html', {'data': data, 'form': form})
 
 
@@ -18,11 +26,40 @@ def save_settings(request):
     #  View: /save
     logger.debug(pformat(request.POST))
     form = PiSwitchSettings(request.POST)
-    logger.info(form)
     if not form.is_valid():
-        logger.info('INVALID FORM --------------')
+        logger.debug('INVALID FORM --------------')
         messages.add_message(request, messages.ERROR, form.errors, extra_tags='danger')
         return redirect('home:show')
     else:
-        logger.info('VALID FORM ++++++++++++++++')
+        logger.debug('VALID FORM ++++++++++++++++')
+        data = {
+            'TWITCH_USER': form.cleaned_data['twitch_user'],
+            'REDIS_PASS': form.cleaned_data['redis_pass'],
+            'REDIS_HOST': form.cleaned_data['redis_host'],
+            'REDIS_PORT': form.cleaned_data['redis_port'],
+            'REDIS_DB': form.cleaned_data['redis_db'],
+        }
+        logger.debug(data)
+        write_settings(data)
+        message = 'Settings successfully saved.'
+        messages.add_message(request, messages.SUCCESS, message, extra_tags='success')
         return redirect('home:show')
+
+
+def get_settings(settings_file='/piswitch/settings.env'):
+    load_dotenv(settings_file, override=True)
+    return {
+        'TWITCH_USER': os.environ.get('TWITCH_USER'),
+        'REDIS_PASS': os.environ.get('REDIS_PASS'),
+        'REDIS_HOST': os.environ.get('REDIS_HOST'),
+        'REDIS_PORT': os.environ.get('REDIS_PORT'),
+        'REDIS_DB': os.environ.get('REDIS_DB'),
+    }
+
+
+def write_settings(data, settings_file='/piswitch/settings.env'):
+    output = ''
+    for key, value in data.items():
+        output += '{}={}\n'.format(key, value)
+    with open(settings_file, 'w+') as f:
+        f.write(output)
